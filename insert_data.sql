065bdad234c83a903d3200f7edf9da9c0909b73f
grey275 <pjvanderpol@gmail.com>
2019-05-21 11:30:41 -0700

    add insert data helper script

---
+++ insert_data.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS famous_people;
+
+CREATE TABLE famous_people (
+ id SERIAL,
+ first_name VARCHAR,
+ last_name VARCHAR,
+ birthdate DATE
+);
+
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Abraham', 'Lincoln', '1809-02-12');
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Mahatma', 'Gandhi', '1869-10-02');
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Paul', 'Rudd', '1969-04-06');
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Paul', 'Giamatti', '1967-06-06');
